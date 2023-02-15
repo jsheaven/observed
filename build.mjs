cba8bbef9c2cfeb9b8e2b8d4594d69230178e1b1
kyr0 <info@aron-homberg.de>
2023-02-15 23:50:32 +0100

    feat: dev builds for test

--- build.mjs
+++ build.mjs
@@ -3,6 +3,7 @@ import { buildForNode } from '@jsheaven/easybuild'
 await buildForNode({
   entryPoint: './src/index.ts',
   outfile: './dist/index.js',
+  debug: process.argv.indexOf('--dev') > -1,
   esBuildOptions: {
     logLevel: 'error',
   },
